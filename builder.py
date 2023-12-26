5254c11b829c861250a8072ffa8ba81573ae4bf7
Andrew Hong <andrew.jwn.hong@gmail.com>
2023-12-25 21:33:34 -0500

    SDL2 download progress

--- builder.py
+++ builder.py
@@ -1,10 +1,12 @@
 __authors__ = "Andrew Hong and Patrick Cao"
 __license__ = "GNU GENERAL PUBLIC LICENSE"
 
-from typing import List
+from typing import List, Union
 from datetime import datetime
-import sys
+import os
 import platform
+import requests
+import zipfile
 
 
 class Log:
@@ -25,16 +27,45 @@ class Log:
 
 
 class Builder:
+    SDL_VERSION = "2.28.5"
+
     def __init__(self) -> None:
         self._operating_system = platform.system()
         self._platform = platform.platform()
 
+    @property
+    def _is_brew_installed(self) -> bool:
+        result = os.system("which brew")
+        return result != "brew not found"
+
+    def _download_tar_file(self, url: str, filename: str) -> Union[None, str]:
+        response = requests.get(url, stream=True)
+
+        out_path = f"bin/{filename}"
+        if response.status_code == 200:
+            with open(out_path, "wb") as f:
+                f.write(response.raw.read())
+
+            return out_path
+        return None
+
+    def _extract_dll_file(self, url: str, filename: str) -> None:
+        out_path = self._download_tar_file(url, filename)
+        with zipfile.ZipFile(out_path, "r") as zip_ref:
+            zip_ref.extractall("./bin")
+
     def run(self) -> None:
-        print("lmao")
+        self._extract_dll_file(
+            f"https://github.com/libsdl-org/SDL/releases/download/release-{self.SDL_VERSION}/SDL2-devel-{self.SDL_VERSION}-mingw.zip",
+            f"sdl-{self.SDL_VERSION}.zip",
+        )
+
         if self._operating_system == "Darwin":  # macOS
-            pass
+            Log.msg("macOS has been detected as your operating system.")
 
         elif self._operating_system == "Windows":
+            Log.msg("Windows has been detected as your operating system.")
+
             pass
 
         else:
@@ -42,3 +73,8 @@ class Builder:
 
 
 Builder().run()
+
+"""
+https://github.com/ocornut/imgui/
+https://github.com/libsdl-org/SDL/
+"""
