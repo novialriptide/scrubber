4a3c32b8c60b50248ce822bcb9c4a07bf8192067
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-01-02 04:08:20 -0500

    code actually works now

--- app/screen.cpp
+++ app/screen.cpp
@@ -50,38 +50,40 @@ int GetEncoderClsid(const WCHAR* format, CLSID* pClsid) {
  * @return IStream* 
  */
 IStream* GetPrimaryMonitorStreamData() {
-  IStream* istream = (IStream *)malloc(sizeof(IStream));
+  IStream* istream = (IStream*)malloc(sizeof(IStream));
   Gdiplus::GdiplusStartupInput gdiplusStartupInput;
   ULONG_PTR gdiplusToken;
 
   Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
-
-  HDC src = GetDC(NULL);
-  HDC dest = CreateCompatibleDC(src);
-
-  int width = GetSystemMetrics(SM_CXSCREEN);
-  int height = GetSystemMetrics(SM_CYSCREEN);
-
-  HBITMAP result = CreateCompatibleBitmap(src, width, height);
-  SelectObject(dest, result);
-
-  // Copy from the desktop device context to the bitmap device context.
-  BitBlt(dest, 0, 0, width, height, src, 0, 0, SRCCOPY);
-
-  Gdiplus::Bitmap bitmap(result, NULL);
-  CLSID clsid;
-  int encoder_clsid = GetEncoderClsid(L"image/jpeg", &clsid);
-  if (encoder_clsid < 0)
   {
-    throw std::runtime_error("JPEG encoder not included with system.");
+    HDC src = GetDC(NULL);
+    HDC dest = CreateCompatibleDC(src);
+
+    int width = GetSystemMetrics(SM_CXSCREEN);
+    int height = GetSystemMetrics(SM_CYSCREEN);
+
+    HBITMAP result = CreateCompatibleBitmap(src, width, height);
+    SelectObject(dest, result);
+
+    // Copy from the desktop device context to the bitmap device context.
+    BitBlt(dest, 0, 0, width, height, src, 0, 0, SRCCOPY);
+
+    Gdiplus::Bitmap bitmap(result, NULL);
+    CLSID clsid;
+    int encoder_clsid = GetEncoderClsid(L"image/jpeg", &clsid);
+    if (encoder_clsid < 0)
+    {
+      throw std::runtime_error("JPEG encoder not included with system.");
+    }
+
+    bitmap.Save(L"screen.jpeg", &clsid, NULL);
+    // bitmap.Save(istream, &clsid, NULL);
+
+    DeleteObject(result);
+    DeleteObject(dest);
+    ReleaseDC(NULL, src);
   }
 
-  bitmap.Save(L"screen.jpeg", &clsid, NULL);
-  bitmap.Save(istream, &clsid, NULL);
-
-  ReleaseDC(NULL, src);
-  DeleteObject(result);
-  DeleteDC(dest);
   Gdiplus::GdiplusShutdown(gdiplusToken);
 
   return istream;
