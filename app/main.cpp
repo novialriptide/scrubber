9ea641c89a8560f4029faa1f535763590674b102
Andrew Hong <andrew.jwn.hong@gmail.com>
2024-01-23 18:43:01 -0500

    Formatting run

--- app/main.cpp
+++ app/main.cpp
@@ -19,8 +19,8 @@ int main(int, char**) {
 
   // Create window with SDL_Renderer graphics context
   SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-  SDL_Window* window = SDL_CreateWindow("scrubber", SDL_WINDOWPOS_CENTERED,
-                                        SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+  SDL_Window* window =
+      SDL_CreateWindow("scrubber", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
   if (window == nullptr) {
     printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
     return -1;
